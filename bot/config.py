68ea6d029a5ed32acb6e0da7751540ed5fd2e6a8
Anton <m@antonz.org>
2023-03-13 20:22:33 +0500

    refactor: config comments

--- bot/config.py
+++ bot/config.py
@@ -12,6 +12,7 @@ telegram_token = config["telegram_token"]
 openai_api_key = config["openai_api_key"]
 
 # The list of Telegram usernames allowed to chat with the bot.
+# If empty, the bot is available to anyone.
 telegram_usernames = set(config["telegram_usernames"])
 
 # Where to store the chat context file.
