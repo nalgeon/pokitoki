116e602c318c83ed96195d685dd7a0652ce17322
Anton <m@antonz.org>
2023-03-25 14:55:07 +0500

    feat: support for fine-tuned models (cli only)

--- bot/cli.py
+++ bot/cli.py
@@ -6,20 +6,32 @@ $ python -m bot.cli "What is your name?"
 """
 
 import asyncio
+import os
 import sys
 import textwrap
 
-from bot.ai.chatgpt import Model
+import bot.ai.chatgpt
+import bot.ai.custom
+import bot.ai.davinci
 
-ai = Model()
+MODEL = os.getenv("OPENAI_MODEL")
 
 
 async def main(question):
     print(f"> {question}")
+    ai = init_model(MODEL)
     answer = await ai.ask(question, history=[])
     lines = textwrap.wrap(answer, width=60)
     for line in lines:
         print(line)
+
+
+def init_model(name):
+    if not name or name == "chatgpt":
+        return bot.ai.chatgpt.Model()
+    if name == "davinci":
+        return bot.ai.davinci.Model()
+    return bot.ai.custom.Model(name)
 
 
 if __name__ == "__main__":
