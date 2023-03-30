4a72fe4ef63a6d6629d166fdb5e42a1d342f5e51
Anton <m@antonz.org>
2023-03-31 01:03:30 +0500

    build: bump version

--- bot/config.py
+++ bot/config.py
@@ -6,7 +6,7 @@ with open("config.yml", "r") as f:
     config = yaml.safe_load(f)
 
 # Bot version.
-version = 70
+version = 76
 
 # Telegram Bot API token.
 telegram_token = config["telegram_token"]
