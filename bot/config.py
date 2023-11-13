5e89e213999c6d95b2c9608a7ccefea07e5990c4
Anton <m@antonz.org>
2023-11-13 15:06:51 +0500

    build: version 182

--- bot/config.py
+++ bot/config.py
@@ -89,7 +89,7 @@ class Config:
     # Config schema version. Increments for backward-incompatible changes.
     schema_version = 4
     # Bot version.
-    version = 176
+    version = 182
 
     def __init__(self, filename: str, src: dict) -> None:
         # Config filename.
