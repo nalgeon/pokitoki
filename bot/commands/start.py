2152355a5967a46fc054bf2a5cfee3cf2f209887
Aleksandr Poliakov <master@artwist.ru>
2025-01-14 11:29:27 +0300

    fixes links im readme

--- bot/commands/start.py
+++ bot/commands/start.py
@@ -16,7 +16,7 @@ class StartCommand:
         if update.effective_user.username not in config.telegram.usernames:
             text = (
                 "Sorry, I don't know you. To setup your own bot, "
-                "visit https://github.com/nalgeon/pokitoki"
+                "visit https://github.com/artwist-polyakov/pokitoki-advanced"
             )
             await update.message.reply_text(text)
             return
